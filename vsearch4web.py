9cc8f23ec89c65554aa93a30a573360fcb92c3df
Łukasz Ruszniak <ruszniakl@uph.edu.pl>
2018-06-21 13:56:34 +0200

    redirect

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,19 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 from vsearch import search4letters
 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello() -> str:
-  return 'Witaj, świecie, tu flask!'
+def hello() -> '302':
+  return redirect('/entry')
 
-@app.route('/search4')
-def do_search() -> str:
-  return str(search4letters('życie, wszechświat i cała reszta','eiru'))
+@app.route('/search4', methods=['POST'])
+def do_search() -> 'html':
+    pharse = request.form['pharse']
+    letters = request.form['letters']
+    title = 'Oto Twoje wyniki:'
+    results = str(search4letters(pharse,letters))
+    return render_template('results.html',
+                            the_title = title,
+                            the_pharse = pharse,
+                            the_letters = letters,
+                            the_results = results,)
 
 @app.route('/entry')
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Witamy na stronie internetowej search4letters!')
 
-app.run()
+app.run(debug=True)
