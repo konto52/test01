b05acf6b9cc85e50fc7f58f800e5102778b2ffb6
Łukasz Ruszniak <ruszniakl@gmail.com>
2018-06-24 22:51:59 +0200

    Dodano obsługę plików i logowanie

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, escape
 from vsearch import search4letters
 
 
 app = Flask(__name__)
 
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -11,6 +14,7 @@ def do_search() -> 'html':
     letters = request.form['letters']
     title = 'Oto Twoje wyniki:'
     results = str(search4letters(pharse,letters))
+    log_request(request,results)
     return render_template('results.html',
                             the_title = title,
                             the_pharse = pharse,
@@ -22,4 +26,19 @@ def do_search() -> 'html':
 def entry_page() -> 'html':
     return render_template('entry.html', the_title='Witamy na stronie internetowej search4letters!')
 
-app.run(debug=True)
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Dane z formularza', 'Adres klienta', "Agent_uzytkownika")
+    return render_template('viewlog.html',
+                           the_title = 'Widok logu',
+                            the_row_titles = titles,
+                            the_data = contents,)
+
+if __name__ == '__main__':
+    app.run(debug=True)
